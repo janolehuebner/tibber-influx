bc05400aed95dee190df111a46b62a7c8f24086d
Jan-Ole Hübner <huebner@jan-ole.de>
2023-09-13 22:53:18 +0200

    setting max runtime to 1h

--- pulse.py
+++ pulse.py
@@ -31,7 +31,7 @@ logger.addHandler(ch)
 
 logger.setLevel(logging.INFO)
 
-__version__ = "v0.1.1"
+__version__ = "v0.1.2"
 logger.info(__version__)
 client = InfluxDBClient(url=URL, token=TOKEN, org=ORG)
 
