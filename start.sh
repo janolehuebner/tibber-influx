8127f0cb0816a38eb122507ba73dbc43691c37b3
Jan-Ole Hübner <jan-ole@huebner.pink>
2024-08-17 22:55:07 +0200

    wrong image

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ printenv > /app/env
 service cron start
 
 # Run your main application script (pulse.py) in the background
-python3 /pulse.py &
+python3 /app/pulse.py &
 
 # Keep the container running and output both cron logs and pulse.py output
 tail -f /var/log/cron.log &
