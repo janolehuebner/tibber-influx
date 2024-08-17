1bfd251502de4c2a6ac72e4b034dbbd15bcfa411
Jan-Ole Hübner <jan-ole@huebner.pink>
2024-08-17 22:21:01 +0200

    aaaaaah

--- delay_price.sh
+++ delay_price.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export PYTHONPATH=/usr/local/lib/python3.12/site-packages
 sleep 2
 
-python3 /get_price.py >> /var/log/cron.log 2>&1
+/usr/local/bin/python3 /get_price.py >> /var/log/cron.log 2>&1
